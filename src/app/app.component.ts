95dd0ae80720871da63abf707569d33449f214b9
Pirathib Mahalingam <pirathib.m@gmail.com>
2024-07-19 13:39:03 +0200

    added font and standard colors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { WorkspaceMenuComponent } from './shared/workspace-menu/workspace-menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, WorkspaceMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/shared/workspace-menu/workspace-menu.component.ts
+++ src/app/shared/workspace-menu/workspace-menu.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @Component({
   selector: 'app-workspace-menu',
   standalone: true,
-  imports: [],
+  imports: [MatSidenavModule, MatExpansionModule],
   templateUrl: './workspace-menu.component.html',
-  styleUrl: './workspace-menu.component.scss'
+  styleUrl: './workspace-menu.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WorkspaceMenuComponent {
-
+  readonly panelOpenState = signal(false);
 }
