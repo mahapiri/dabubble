fdec0207846096646bb4450706a56cd705abdebf
SusanneRobledo <susanne.robledo@web.de>
2024-07-20 12:05:23 +0200

    channel HTML & first CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { WorkspaceMenuComponent } from './shared/workspace-menu/workspace-menu.component';
+import { ChannelComponent } from './main-board/channel/channel.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, WorkspaceMenuComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    WorkspaceMenuComponent,
+    ChannelComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'dabubble';
--- src/app/main-board/channel/channel.component.ts
+++ src/app/main-board/channel/channel.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-channel',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './channel.component.html',
-  styleUrl: './channel.component.scss'
+  styleUrl: './channel.component.scss',
 })
-export class ChannelComponent {
-
-}
+export class ChannelComponent {}
