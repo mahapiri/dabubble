0894bfd0e7c3fdbdc5bce2161bc670c4be8678bc
Bort190 <heiko.nevoigt@googlemail.com>
2024-07-20 10:51:37 +0200

    login-component

--- src/app/login/log-in/log-in.component.ts
+++ src/app/login/log-in/log-in.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card'; 
 
 @Component({
   selector: 'app-log-in',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.scss'
 })
