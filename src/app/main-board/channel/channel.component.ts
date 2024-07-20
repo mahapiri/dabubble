b0e8f72155a01c25d3ce0aa205d60a40bc6dc476
SusanneRobledo <susanne.robledo@web.de>
2024-07-20 14:45:55 +0200

    channel button html & css

--- src/app/main-board/channel/channel.component.ts
+++ src/app/main-board/channel/channel.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-channel',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, MatDividerModule],
   templateUrl: './channel.component.html',
   styleUrl: './channel.component.scss',
 })
