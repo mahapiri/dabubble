049d1d52024cd54196fb903345e7e8dfa88abb85
Pirathib Mahalingam <pirathib.m@gmail.com>
2024-07-22 01:47:57 +0200

    edited workspace menu

--- src/app/shared/workspace-menu/workspace-menu.component.ts
+++ src/app/shared/workspace-menu/workspace-menu.component.ts
@@ -1,15 +1,100 @@
-import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import {MatExpansionModule} from '@angular/material/expansion';
+import { Component, ChangeDetectionStrategy, signal, ViewChild } from '@angular/core';
+import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { User } from '../../models/user.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-workspace-menu',
   standalone: true,
-  imports: [MatSidenavModule, MatExpansionModule],
+  imports: [MatSidenavModule, MatExpansionModule, MatButtonModule, MatIconModule, CommonModule],
   templateUrl: './workspace-menu.component.html',
   styleUrl: './workspace-menu.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
+
 export class WorkspaceMenuComponent {
+  hover: boolean = false;
+  open: boolean = false;
+  clickedChannel: boolean = false;
+  clickedMessage: boolean = false;
+  clickedUser: boolean = false;
+  openChannel: boolean = false;
+  openDm: boolean = false;
+
+  user: User[] = [
+    {
+      name: 'Federik Beck (Du)', 
+      img: '../../../assets/img/character1.png',
+      status: 'online'
+    },
+    {
+      name: 'Sofia Müller', 
+      img: '../../../assets/img/character2.png',
+      status: 'online'
+    },
+    {
+      name: 'Noah Braun', 
+      img: '../../../assets/img/character3.png',
+      status: 'offline'
+    },
+    {
+      name: 'Elise Roth', 
+      img: '../../../assets/img/character4.png',
+      status: 'offline'
+    },
+    {
+      name: 'Elias Neumann', 
+      img: '../../../assets/img/character5.png',
+      status: 'online'
+    },
+    {
+      name: 'Steffen Hoffmann', 
+      img: '../../../assets/img/character6.png',
+      status: 'online'
+    },
+
+  ]
+
   readonly panelOpenState = signal(false);
+  @ViewChild('drawer') drawer!: MatDrawer;
+
+  toggle() {
+    this.drawer.toggle();
+    this.open = !this.open;
+    this.hover = true;
+  }
+
+  hoverTrue() {
+    this.hover = true;
+  }
+
+  hoverFalse() {
+    this.hover = false;
+  }
+
+  newMessage() {
+    this.clickedMessage = !this.clickedMessage;
+  }
+
+  createChannel() {
+    this.clickedChannel = !this.clickedChannel;
+  }
+
+  openChannels() {
+    this.openChannel = !this.openChannel;
+  }
+
+  openDirectMessages() {
+    this.openDm = !this.openDm;
+  }
+
+  clickedProfile(i: number) {
+    this.clickedUser = !this.clickedUser;
+    let id = i;
+    document.getElementById(`profile-${id}`)?.classList.toggle('bold-user');
+  }
+
 }
