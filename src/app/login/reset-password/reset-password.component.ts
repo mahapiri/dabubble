e8cf928c8aadc22551c4e9c320267f6786cc8104
Bort190 <heiko.nevoigt@googlemail.com>
2024-07-22 14:29:27 +0200

    login cards

--- src/app/login/reset-password/reset-password.component.ts
+++ src/app/login/reset-password/reset-password.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-reset-password',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatIconModule, ReactiveFormsModule],
   templateUrl: './reset-password.component.html',
-  styleUrl: './reset-password.component.scss'
+  styleUrl: './reset-password.component.scss',
 })
-export class ResetPasswordComponent {
+export class ResetPasswordComponent {}
 
-}
+
+
+
--- src/app/login/send-mail/send-mail.component.ts
+++ src/app/login/send-mail/send-mail.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-send-mail',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatIconModule, ReactiveFormsModule],
   templateUrl: './send-mail.component.html',
-  styleUrl: './send-mail.component.scss'
+  styleUrl: './send-mail.component.scss',
 })
-export class SendMailComponent {
-
-}
+export class SendMailComponent {}
