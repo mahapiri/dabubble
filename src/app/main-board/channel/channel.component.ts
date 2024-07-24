5cca1c9a3e35dbc662f743d315053ab4ad75a6fe
SusanneRobledo <susanne.robledo@web.de>
2024-07-24 09:24:13 +0200

    workspace css

--- src/app/main-board/channel/channel.component.ts
+++ src/app/main-board/channel/channel.component.ts
@@ -1,10 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-channel',
@@ -16,9 +17,19 @@ import { MatListModule } from '@angular/material/list';
     MatDividerModule,
     MatInputModule,
     MatListModule,
+    MatDialogModule,
   ],
   templateUrl: './channel.component.html',
   styleUrl: './channel.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ChannelComponent {}
+export class ChannelComponent {
+  /* readonly dialog = inject(MatDialog);
+
+  openDialog() {
+    const dialogRef = this.dialog.open();
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  } */
+}
