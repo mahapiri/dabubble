d430dc420f401200dde99e0fd2c488bef2501761
SusanneRobledo <susanne.robledo@web.de>
2024-08-01 18:18:44 +0200

    date and time formatted

--- src/app/channel/channel/channel-message/channel-message.component.ts
+++ src/app/channel/channel/channel-message/channel-message.component.ts
@@ -6,6 +6,7 @@ import { MatListModule } from '@angular/material/list';
 import { ChannelService } from '../../../services/channel.service';
 import { CommonModule } from '@angular/common';
 import { ChannelMessage } from '../../../../models/channel.class';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-channel-message',
@@ -28,5 +29,28 @@ export class ChannelMessageComponent {
 
   openThread() {
     this.clickedAnswer.emit(true);
+  }
+
+  formatTime(time: string): string {
+    const [hours, minutes] = time.split(':');
+    return `${hours}:${minutes}`;
+  }
+
+  formatDateString(dateString: string): string {
+    const [day, month, year] = dateString.split('.').map(Number);
+    const inputDate = new Date(year, month - 1, day);
+    const today = new Date();
+
+    const todayDate = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate()
+    );
+
+    if (inputDate.toDateString() === todayDate.toDateString()) {
+      return 'Heute';
+    }
+
+    return formatDate(inputDate, 'EEEE, dd.MM.yyyy', 'de');
   }
 }
