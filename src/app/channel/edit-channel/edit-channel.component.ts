f002d2bd337e8b499dd0afd7f0419cd66e48a03a
Bort190 <heiko.nevoigt@googlemail.com>
2024-08-06 16:42:21 +0200

    edit channel

--- src/app/channel/edit-channel/edit-channel.component.ts
+++ src/app/channel/edit-channel/edit-channel.component.ts
@@ -1,29 +1,40 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { Channel } from '../../../models/channel.class';
+import { doc, Firestore, updateDoc } from '@angular/fire/firestore';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-edit-channel',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, FormsModule, CommonModule],
   templateUrl: './edit-channel.component.html',
   styleUrl: './edit-channel.component.scss'
 })
 export class EditChannelComponent {
   editing: boolean = false;
+  firestore: Firestore = inject(Firestore);
   @Input() channel!: Channel;
   @Output() channelClosed = new EventEmitter<boolean>();
 
-
-
-  ngOnInit(){
-    
+  edit() {
+    this.editing = !this.editing;
+    this.saveChanges()
   }
-  closeChannel(){
+
+  async saveChanges() {
+    if (this.channel.channelID) {
+      await updateDoc(doc(this.firestore, "channels", this.channel.channelID), {
+        channelName: this.channel.channelName,
+        description: this.channel.description
+      });
+    }
+  }
+
+  closeChannel() {
     this.channelClosed.emit(false)
   }
 
-  edit() {
-    this.editing = !this.editing;
-  }
+
 }
