56f9c237b145463fe6444b69d8c1e25d27a2c63d
Bort190 <heiko.nevoigt@googlemail.com>
2024-08-10 11:31:27 +0200

    signup validation

--- src/app/login/sign-up/sign-up.component.ts
+++ src/app/login/sign-up/sign-up.component.ts
@@ -4,12 +4,13 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { AuthService } from './../../services/auth.service';
 import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [MatCardModule, MatIconModule, ReactiveFormsModule, MatIcon, RouterLink],
+  imports: [MatCardModule, MatIconModule, ReactiveFormsModule, MatIcon, RouterLink, CommonModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
@@ -17,23 +18,42 @@ export class SignUpComponent {
   formbuilder: FormBuilder = inject(FormBuilder);
   authService: AuthService = inject(AuthService);
   passwordVisible: boolean = false;
+  passwordErrorMessage: string = "";
+  mailerrorMessage: string = "";
+  nameErrorMessage: string = "";
+  invalidName: boolean = false;
+  invalidMail: boolean = false;
+  invalidPassword: boolean = false;
   constructor(private router: Router) { }
 
   userForm = this.formbuilder.group({
     userName: ["", Validators.required],
     userEmail: ["", [Validators.required, Validators.email]],
-    userPassword: ["", Validators.required]
+    userPassword: ["", [Validators.required, Validators.minLength(6)]]
   })
 
   async onSubmit() {
-
+    this.invalidName = false;
+    this.invalidMail = false;
+    this.invalidPassword = false;
     if (this.userForm.valid) {
       this.authService.usermail = this.userForm.value.userEmail || "";
       this.authService.username = this.userForm.value.userName || "";
       this.authService.userpassword = this.userForm.value.userPassword || "";
       this.router.navigate(['/choose-avatar']);
     }
-
+    else if (this.userForm.get('userName')?.invalid) {
+      this.invalidName = true;
+      this.nameErrorMessage = "Bitte geben Sie einen Namen ein";
+    }
+    else if (this.userForm.get('userEmail')?.invalid) {
+      this.invalidMail = true;
+      this.mailerrorMessage = "Bitte geben Sie eine gültige Mailadresse ein";
+    }
+    else if (this.userForm.get('userPassword')?.invalid) {
+      this.invalidPassword = true;
+         this.passwordErrorMessage = "Ihr Passwort sollte mindestens 6 Zeichen lang sein";
+    }
   }
 
   showPassword() {
