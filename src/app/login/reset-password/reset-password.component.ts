937a39321993c3f380f4167acbcdef07254d6d5a
Bort190 <heiko.nevoigt@googlemail.com>
2024-08-20 18:48:46 +0200

    reset password working

--- src/app/login/reset-password/reset-password.component.ts
+++ src/app/login/reset-password/reset-password.component.ts
@@ -9,49 +9,101 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
-import { Auth, updatePassword } from '@angular/fire/auth';
+import {
+  Auth,
+  confirmPasswordReset,
+  signInWithEmailAndPassword,
+  updatePassword,
+  verifyPasswordResetCode,
+} from '@angular/fire/auth';
 import { UserService } from '../../services/user.service';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-reset-password',
   standalone: true,
-  imports: [MatCardModule, MatIconModule, ReactiveFormsModule, RouterLink],
+  imports: [MatCardModule, MatIconModule, ReactiveFormsModule, RouterLink, CommonModule],
   templateUrl: './reset-password.component.html',
   styleUrl: './reset-password.component.scss',
 })
 export class ResetPasswordComponent {
   constructor(private router: Router, private auth: Auth) { }
 
-  formbuilder: FormBuilder = inject(FormBuilder)
+
+  formbuilder: FormBuilder = inject(FormBuilder);
   authService: AuthService = inject(AuthService);
-  userService: UserService = inject(UserService)
+  userService: UserService = inject(UserService);
+  actionCode: string | null = '';
+  passwordsNotMatching: boolean = false;
 
   userForm = this.formbuilder.group({
-    newPassword: ["", [Validators.required, Validators.minLength(6)]],
-    newPasswordConfirm: ["", [Validators.required, Validators.minLength(6)]]
-  })
+    newPassword: ['', [Validators.required, Validators.minLength(6)]],
+    newPasswordConfirm: ['', [Validators.required, Validators.minLength(6)]],
+  });
 
-  ngOnInit() {
-    console.log(this.userService.currentUser$, this.authService.currentUser, this.auth.currentUser);
-
+  /**
+   * get action code from reset-password email
+   */
+  ngOnInit(): void {
+    this.actionCode = this.getParameterByName('oobCode');
   }
+
+  /**
+   * extracts the oobCode from the email-url
+   * @param name 
+   * @returns oob-code
+   */
+  getParameterByName(name: string): string | null {
+    const url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
+    const results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+  }
+
+  /**
+   * uses the action code to reset the password 
+   */
   async onSubmit() {
-
-    this.authService.userpassword = this.userForm.value.newPassword || '';
-
-    /*    updatePassword(user, newPassword).then(() => {
-         // Update successful.
-       }).catch((error) => {
-         // An error ocurred
-         // ...
-       }); */
-    this.router.navigate(['/main-window']);
-
+    if (this.actionCode) {
+      this.handleResetPassword(this.auth, this.actionCode);
+    }
   }
 
+  /**
+   * verifies the action code; resets the password with the one the user has typed in; logs in the user with the new password
+   * @param auth 
+   * @param actionCode 
+   */
+  handleResetPassword(auth: Auth, actionCode: string) {
+    verifyPasswordResetCode(auth, actionCode).then((email) => {
+      const newPassword = this.userForm.value.newPassword;
+      this.checkMatchingPassword();
+      if (newPassword && !this.passwordsNotMatching) {
+        confirmPasswordReset(auth, actionCode, newPassword).then(async (resp) => {
+          await signInWithEmailAndPassword(this.auth, email, newPassword);
+          this.router.navigate(['/main-window']);
+        })
+          .catch((error) => {
+            console.log('Link expired. Please try again');
+          });
+      }
+    })
+      .catch((error) => {
+        console.log('Link expired or action Code not correct. Please try again');
+      });
+  }
+
+
+  checkMatchingPassword(){
+    if (this.userForm.value.newPassword === this.userForm.value.newPasswordConfirm) {
+      this.passwordsNotMatching = false
+    }
+    else{
+      this.passwordsNotMatching = true
+    }
+  }
 }
-
-
-
-
