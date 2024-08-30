02efc86794d430412a87af2236e7bcf59476e24c
Pirathib Mahalingam <pirathib.m@gmail.com>
2024-08-30 11:52:56 +0200

    edited new message comp

--- src/app/new-message/new-message-header/new-message-header.component.ts
+++ src/app/new-message/new-message-header/new-message-header.component.ts
@@ -1,22 +1,39 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { ClickOutsideDirective } from '../../directive/click-outside.directive';
+import { user } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-new-message-header',
   standalone: true,
   imports: [
     MatCardModule,
-    CommonModule
+    CommonModule,
+    MatIconModule,
+    FormsModule,
+    ClickOutsideDirective
   ],
   templateUrl: './new-message-header.component.html',
   styleUrl: './new-message-header.component.scss'
 })
 export class NewMessageHeaderComponent {
   inputActive: boolean = false;
+  usersearch: boolean = false;
+  channelsearch: boolean = false;
+  searchword: string = '';
+  @Output() searchwordValue = new EventEmitter<string>();
 
 
-  startSearch() {
-    this.inputActive = true;
+  openResults() {
+    this.inputActive = this.searchword.trim().length > 0;
+    this.usersearch = this.searchword.startsWith('@');
+    this.channelsearch = this.searchword.startsWith('#');
+  }
+
+  closeResults() {
+    this.inputActive = false;
   }
 }
