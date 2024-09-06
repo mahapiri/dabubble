836200be213aa81a2a2065382f0d7a90d02367b1
Pirathib Mahalingam <pirathib.m@gmail.com>
2024-09-06 22:23:50 +0200

    open profile at dm

--- src/app/direct-message/direct-message-header/direct-message-header.component.ts
+++ src/app/direct-message/direct-message-header/direct-message-header.component.ts
@@ -5,6 +5,7 @@ import { UserService } from '../../services/user.service';
 import { User } from '../../../models/user.class';
 import { Subscription } from 'rxjs';
 import { DirectMessageService } from '../../services/direct-message.service';
+import { SharedService } from '../../services/shared.service';
 
 @Component({
   selector: 'app-direct-message-header',
@@ -19,8 +20,11 @@ import { DirectMessageService } from '../../services/direct-message.service';
 export class DirectMessageHeaderComponent implements OnInit, OnDestroy {
   public userService: UserService = inject(UserService);
   private userSubscription: Subscription = new Subscription();
+  private currentUserSubscription: Subscription = new Subscription();
   public directMessageService: DirectMessageService = inject(DirectMessageService);
+  private sharedService: SharedService = inject(SharedService);
   profile: Partial<User> = {};
+  currentuser: User[] = [];
 
 
   /**
@@ -37,15 +41,38 @@ export class DirectMessageHeaderComponent implements OnInit, OnDestroy {
         state: profile?.state
       };
     });
+
+    this.currentUserSubscription = this.userService.currentUser$.subscribe((user) => {
+      if (user) {
+        this.currentuser.push(user);
+      }
+
+    })
   }
 
 
-/**
- * unsubscribes the profile service while it is not anymore in use
- *
- * @memberof DirectMessageHeaderComponent
- */
+  /**
+   * unsubscribes the profile service while it is not anymore in use
+   *
+   * @memberof DirectMessageHeaderComponent
+   */
   ngOnDestroy(): void {
     this.userSubscription.unsubscribe();
+    this.currentUserSubscription.unsubscribe();
+  }
+
+  /**
+   * opens the profile popup
+   * @param event 
+   */
+  openProfile(event: Event) {
+    event.stopPropagation();
+    let userId = `${this.profile.userId}`;
+    if (userId == this.currentuser[0].userId) {
+      console.log('open my-profile'); // logik muss noch hinzugefügt werden
+      this.sharedService.openProfile(userId); // vorrübergehend drin
+    } else {
+      this.sharedService.openProfile(userId);
+    }
   }
 }
