2d1ab8d66637b3fb824103368c5c2355f0dc406c
Pirathib Mahalingam <pirathib.m@gmail.com>
2024-09-09 20:29:35 +0200

    edited reaction bar + edit message

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -228,5 +228,6 @@ export class HeaderComponent implements OnInit {
     this.chatService.setIsChannel(true);
     this.sharedService.setSelectProfile(false);
     this.sharedService.resetSelectedUserIndex();
+    this.chatService.handleWindowChangeOnMobile();
   }
 }
