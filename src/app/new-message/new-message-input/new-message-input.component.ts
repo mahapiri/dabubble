1004243926244132ff2ce65ea000016a5bf7e41f
Pirathib Mahalingam <pirathib.m@gmail.com>
2024-09-13 11:18:55 +0200

    add tagging to new message

--- src/app/new-message/new-message-input/new-message-input.component.ts
+++ src/app/new-message/new-message-input/new-message-input.component.ts
@@ -63,6 +63,7 @@ export class NewMessageInputComponent implements OnInit, OnDestroy {
   isEmoji: boolean = false;
   notOpen: boolean = true;
   isTag: boolean = false;
+  findTag: boolean = false;
   searchword: string = '';
 
   constructor() {
@@ -95,9 +96,17 @@ export class NewMessageInputComponent implements OnInit, OnDestroy {
    * add member to message field
    */
   addMemberToMessage(username: string) {
-    const mention = `@${username} `;
-    if (!this.messageText.includes(mention)) {
+    let mention = '';
+    if (this.findTag) {
+      mention = `${username} `;
+      this.messageText += `${mention}`;
+    } else if (!this.messageText.includes(mention) && !this.findTag) {
+      mention = `@${username} `;
       this.messageText += ` ${mention}`;
+    }
+
+    if (!this.messageText.includes(mention)) {
+
     }
   }
 
@@ -171,7 +180,7 @@ export class NewMessageInputComponent implements OnInit, OnDestroy {
     event.stopPropagation();
     await this.checkPictureUpload();
 
-    if (!this.messageText.trim()) {
+    if (!this.messageText.trim() && !this.uploadService.dmFileChosen) {
       console.warn('The message field is empty. Please type a message!');
     } else {
       this.messageIdSubscription = this.newMessageService.messageId$
@@ -272,11 +281,36 @@ export class NewMessageInputComponent implements OnInit, OnDestroy {
     this.isTag = !this.isTag;
   }
 
-  
+
   /**
    * close tagging popup
    */
   closePopup() {
     this.isTag = false;
+    this.findTag = false;
+  }
+
+
+  /**
+  * opens tagging
+  */
+  openTagging() {
+    const lastChar = this.messageText.slice(-1);
+
+    if (!this.findTag) {
+      if (lastChar === '@') {
+        this.findTag = true;
+        this.isTag = true;
+      }
+      if (lastChar === '#') {
+        this.findTag = true;
+        this.isTag = true;
+      }
+    } else if (this.findTag) {
+      if (!this.messageText.includes('@') && !this.messageText.includes('#')) {
+        this.isTag = false;
+        this.findTag = false;
+      }
+    }
   }
 }
