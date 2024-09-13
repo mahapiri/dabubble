1ba482a44bd9b0db6d916f160f67ce072ddebd96
Pirathib Mahalingam <pirathib.m@gmail.com>
2024-09-13 11:23:06 +0200

    add tagging to channel

--- src/app/channel/channel/channel-new-message-input/channel-new-message-input.component.ts
+++ src/app/channel/channel/channel-new-message-input/channel-new-message-input.component.ts
@@ -52,8 +52,9 @@ export class ChannelNewMessageInputComponent implements OnInit {
   isEmoji: boolean = false;
   notOpen: boolean = true;
   isTag: boolean = false;
+  findTag: boolean = false;
 
-  constructor(private channelMessageService: ChannelMessageService) {}
+  constructor(private channelMessageService: ChannelMessageService) { }
 
   /**
    * subscribes selected member
@@ -74,7 +75,14 @@ export class ChannelNewMessageInputComponent implements OnInit {
   addMemberToMessage(username: string) {
     const mention = `@${username} `;
     if (!this.messageText.includes(mention)) {
-      this.messageText += ` ${mention}`;
+      let mention = '';
+      if (this.findTag) {
+        mention = `${username} `;
+        this.messageText += `${mention}`;
+      } else if (!this.messageText.includes(mention) && !this.findTag) {
+        mention = `@${username} `;
+        this.messageText += ` ${mention}`;
+      }
     }
   }
 
@@ -128,6 +136,7 @@ export class ChannelNewMessageInputComponent implements OnInit {
    */
   closePopup() {
     this.isTag = false;
+    this.findTag = false;
   }
 
   /**
@@ -166,5 +175,29 @@ export class ChannelNewMessageInputComponent implements OnInit {
   onEmojiSelected(emoji: string) {
     this.messageText += emoji;
     this.closeEmojiSet();
+  }
+
+
+  /**
+* opens tagging
+*/
+  openTagging() {
+    const lastChar = this.messageText.slice(-1);
+
+    if (!this.findTag) {
+      if (lastChar === '@') {
+        this.findTag = true;
+        this.isTag = true;
+      }
+      if (lastChar === '#') {
+        this.findTag = true;
+        this.isTag = true;
+      }
+    } else if (this.findTag) {
+      if (!this.messageText.includes('@') && !this.messageText.includes('#')) {
+        this.isTag = false;
+        this.findTag = false;
+      }
+    }
   }
 }
